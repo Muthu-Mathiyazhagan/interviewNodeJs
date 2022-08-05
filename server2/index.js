14d9f629e2bb8ad271582d69f405bc4a516b24ae
muthu-mathiyazhagan <muthumathiyazhagan@gmail.com>
2022-08-05 11:26:57 +0530

    /ask api implemented Successfully'

--- server2/index.js
+++ server2/index.js
@@ -1,77 +1,75 @@
-const express = require("express");
-const app = express();
-const router = express.Router();
-let response;
+const express = require('express')
+const app = express()
+const router = express.Router()
+let response
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+const randomStatus = ['Yes', 'No', 'na']
 
+app.use('/', router)
 
-const PORT = 3010;
+const PORT = 3010
 
 app.listen(PORT, () => {
-    console.log(`Connected to Server ${PORT}`);
-});
+  console.log(`Connected to Server ${PORT}`)
+})
 
-router.post('/ask',(req,res)=>{
-    console.log("Ask Called");
+router.post('/ask', async (req, res) => {
+  console.log('Ask Called')
 
-    let typeOfAge = typeof(req.body.age);
+  console.log(`Req.Body : ${req.body.age}`)
 
-    // If the Age  number error code 00
-    let requestID = new Date().now();
-    if (typeOfAge == 'number') {
-        response = {
-            "name": req.body.name,
-            "age": req.body.age,
-            "mobile": req.body.mobile,
-            "msg": req.body.msg,
-            "reqId": requestID,
-            "reqStatus":{
-                "Status":"Success"
-            }
-        }
-        server2to1(requestID,response);
+  // If the Age  number error code 00
+  let requestID = new Date().getTime()
+  console.log(`randomStatus[requestID % 3] : ${randomStatus[requestID % 3]}`)
+  console.log(`requestID : ${requestID}`)
+  if (typeof req.body.age == 'number') {
+    response = {
+      name: req.body.name,
+      age: req.body.age,
+      mobile: req.body.mobile,
+      msg: req.body.msg,
+      reqId: requestID,
+      reqStatus: {
+        Status: 'Success'
+      }
+    }
+    server2to1(requestID, response)
 
-        return res.send(response).status(200);
-        
-    }else {
-        // handle if the Input is not Number
+    return res.send(response).status(200)
+  } else {
+    // handle if the Input is not Number
+    console.log(`Its Must be String: ${typeof req.body.age}`)
 
-        response = {
-
-            "name": req.body.name,
-            "age": req.body.age,
-            "mobile": req.body.mobile,
-            "msg": req.body.msg,
-            "reqId": requestID,
-            "reqStatus":{
-                "Status":"error":{
-                    "code":"00",
-                    "msg":"the given age must be Number only"
-                }
-            }
-        }
-        server2to1(requestID,response);
-        return res.send("The Give Input is Wrong. Try again with correct Structure").status(400);
-
+    response = {
+      name: req.body.name,
+      age: req.body.age,
+      mobile: req.body.mobile,
+      msg: req.body.msg,
+      reqId: requestID,
+      reqStatus: {
+        Status: 'Failure',
+        code : '00',
+        msg : 'Age Field Must be String.!'
+      }
     }
 
+    console.log(`response : ${response}`)
 
-    
+    server2to1(requestID, response)
+    return res
+      .send(response)
+      .status(200)
+  }
 });
 
-const randonStatus = ['Yes','No','na'];
+async function server2to1 (requestID) {    
+  setTimeout(() => {
+    response.reqId = requestID
+    response.response_s = randomStatus[requestID % 3]
 
-async function server2to1(requestID) {
-    setTimeout(() => {
+    // The Response mentioned here is Req to Server One
 
-        response.reqId = requestID;
-        response.response_s = randonStatus[requestID % 3];
-
-
-        // The Response mentioned here is Req to Server One
-       
-
-        // call the Server 1 by Only One Response
-    }, 2000);
-    
+    // call the Server 1 by Only One Response
+  }, 2000)
 }
-
